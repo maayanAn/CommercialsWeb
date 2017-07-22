4d17f781d6b88d9dbff50d76766f8ed230be40cc
maayanAn <maayanangel@gmail.com>
2017-07-22 12:15:19 +0300

    test1

--- finalProject/protractor.conf.js
+++ finalProject/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+//const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -19,10 +19,10 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+  // onPrepare() {
+  //   require('ts-node').register({
+  //     project: 'e2e/tsconfig.e2e.json'
+  //   });
+  //   jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  // }
 };
